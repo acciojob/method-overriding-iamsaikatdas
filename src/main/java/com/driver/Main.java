8d812a9c840e8c515db5b68bdfa03163dede0713
Saikat Das <102199984+iamsaikatdas@users.noreply.github.com>
2023-01-12 13:59:14 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      B obj = new B();
+      System.out.println(obj.meth());
+ } 
+
+class A{
+  public String meth(){
+       return "I am in A's meth";
+  }
+}
+class B extends A{
+  public String meth(){
+    return "I am in B's meth";
+  }
 }
