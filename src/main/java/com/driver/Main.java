046f30204e3676057a3b8146afa8c21cac49624a
Saikat <saikatdas9547@gmail.com>
2023-01-13 09:17:54 +0530

    Project methodoverloading is done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,19 @@
 package com.driver;
 
 public class Main {
-  public void main(String[] args){
-      B obj = new B();
-      System.out.println(obj.meth());
- } 
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.meth());
+    }
+}
+    class A{
+      public String meth(){
+           return "I am in A's meth";
+      }
+    }
+    class B extends A{
+      public String meth(){
+        return "I am in B's meth";
+      }
+    }
 
-class A{
-  public String meth(){
-       return "I am in A's meth";
-  }
-}
-class B extends A{
-  public String meth(){
-    return "I am in B's meth";
-  }
-}
-}
