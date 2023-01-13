e02c255da447f4ce2340becd2b5b043fc9909fb5
Saikat <saikatdas9547@gmail.com>
2023-01-13 20:07:24 +0530

    8th times

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,15 +5,17 @@ public class Main {
         B obj = new B();
         obj.meth();
     }
-}
-class A{
-    public String meth(){
-        return "Invoking method from class A";
+
+    public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
     }
-}
-class B extends A{
-    @Override
-    public String meth() {
-        return "Method is overridden in Extendend class B";
+
+    public static class B extends A {
+        @Override
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
     }
 }
